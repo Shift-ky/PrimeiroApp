b9f2dfa86999f857e30fb6f084b82b5c8836d2ae
Robson Paiva <103571578+Shift-ky@users.noreply.github.com>
2022-12-16 14:30:50 -0300

    Finalização do aplicativo

--- app/src/main/java/com/first/primeiroapp/MainActivity.java
+++ app/src/main/java/com/first/primeiroapp/MainActivity.java
@@ -6,7 +6,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
+    Random randon = new Random();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,9 +17,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void alterarTexto(View view){
-        TextView texto = findViewById(R.id.exibicao);
-        texto.setText("Robson Paiva");
+    public void numeroAleatorio(View view){
+        TextView texto = findViewById(R.id.aleatorio);
 
+        int numero = randon.nextInt(10);
+        texto.setText("O numero selecionado foi: "+numero);
     }
 }
